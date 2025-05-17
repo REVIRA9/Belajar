88e9f571f6727a7b1a80d303e483779972840b7c
REVIRA9 <96121841+REVIRA9@users.noreply.github.com>
2025-05-17 17:35:47 +0700

    Add files via upload

---
+++ Freelance/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"Freelance/function"
+	"fmt"
+)
+
+func main(){
+	fmt.Println("hello")
+	function.Menu()
+	fmt.Println("Test")
+	fmt.Println("Test")
+	fmt.Println("Test")
+	fmt.Println("1")
+	fmt.Println("1")
+	fmt.Println("1")
+	fmt.Println("1")
+}
